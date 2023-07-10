1099b2776ce1f31a3d0f150325febb986c2658cd
KirillChern <kirillchernliko@gmail.com>
2023-07-10 20:31:22 +0300

    add library

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchImages } from './fetchImages';
